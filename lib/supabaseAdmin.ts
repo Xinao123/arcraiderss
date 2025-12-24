d6952b3f325735226f674d77fb236f2935d40ae0
Xinao123 <xinaoguerrilheiro@gmail.com>
2025-12-23 23:32:36 -0300

    Update supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -3,7 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const url = process.env.SUPABASE_URL!;
 const serviceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
-// client admin: só usar no server (route handlers / server actions)
 export const supabaseAdmin = createClient(url, serviceKey, {
   auth: { persistSession: false },
 });
